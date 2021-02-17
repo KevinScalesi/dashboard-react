06e7ba883d88dfeec8dd93d119d059049ba0c2c6
KevinScalesi <kevin_scalesi@yahoo.com.ar>
2021-02-16 22:42:54 -0300

    update App component to statefull and get total user, total products and amount from DB

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 import './app.css';
 import Head from './components/Head'
 import SideBar from './components/SideBar'
@@ -10,154 +11,184 @@ import Footer from './components/Footer'
 import Table from './components/Table'
 
 
-function App() {
-
-  let categories = ["Category01","Category02","Category03","Category04","Category05","Category06"]
-  let smallBoxes = [
-    {
-      title: "Products in Data Base",
-      icon: "fa-clipboard-list",
-      value: 135,
-      border: "border-left-primary"
-    },
-    {
-      title: "Amount in products",
-      icon: "fa-dollar-sign",
-      value: "$546.456",
-      border: "border-left-success"
-    },
-    {
-      title: "Users quantity",
-      icon: "fa-user-check",
-      value: 38,
-      border: "border-left-warning"
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      totalUsers: 0,
+      totalPorducts: 0,
+      amountProducts: "",
     }
-  ]
+  }
 
-  let products=[
-    {
-      Name:"Tiger Nixon",
-      Description:"System Architect",
-      Price:320.800,
-      Categories:["Category 01","Category 02","Category 03"],
-      Colors:[
-        {
-          Color:"Red",
-          Clase:"text-danger"
-        },
-        {
-          Color:"Blue",
-          Clase:"text-primary"
-        },
-        {
-          Color:"Green",
-          Clase:"text-success"
-        }
-      ],
-      Stock:245
-    },
-    {
-      Name:"Jane Doe",
-      Description:"Fullstack developer",
-      Price:320.800,
-      Categories:["Category 01","Category 02","Category 03"],
-      Colors:[
-        {
-          Color:"Red",
-          Clase:"text-danger"
-        },
-        {
-          Color:"Blue",
-          Clase:"text-primary"
-        },
-        {
-          Color:"Green",
-          Clase:"text-success"
-        }
-      ],
-      Stock:245
-    }
-  ]
-  let columns = Object.keys(products[0])
+  apiCall(url,consecuencia,key){
+    fetch(url)
+      .then(res => res.json())
+      .then(data => consecuencia(data,key))
+      .catch(error => console.log(error))
+  }
 
-  return (
-    <div className="App">
+  componentDidMount(){
+    this.apiCall("http://localhost:3001/api/users/total", this.updateState,"totalUsers")
+    this.apiCall("http://localhost:3001/api/products/total", this.updateState,"totalProducts")
+    this.apiCall("http://localhost:3001/api/products/amount", this.updateState,"amountProducts")
+  }
 
-      <Head />
+  updateState = (data, key) => {
+      this.setState({
+        ...this.state,
+        [key]: data.data
+      })
+  }
 
-      <body>
+  render(){
+    let categories = ["Category01","Category02","Category03","Category04","Category05","Category06"]
+    let smallBoxes = [
+      {
+        title: "Products in Data Base",
+        icon: "fa-clipboard-list",
+        value: this.state.totalProducts,
+        border: "border-left-primary"
+      },
+      {
+        title: "Amount in products",
+        icon: "fa-dollar-sign",
+        value: this.state.amountProducts,
+        border: "border-left-success"
+      },
+      {
+        title: "Users quantity",
+        icon: "fa-user-check",
+        value: this.state.totalUsers,
+        border: "border-left-warning"
+      }
+    ]
 
-        <div id="wrapper">
-          
-          <SideBar />
+    let products=[
+      {
+        Name:"Tiger Nixon",
+        Description:"System Architect",
+        Price:320.800,
+        Categories:["Category 01","Category 02","Category 03"],
+        Colors:[
+          {
+            Color:"Red",
+            Clase:"text-danger"
+          },
+          {
+            Color:"Blue",
+            Clase:"text-primary"
+          },
+          {
+            Color:"Green",
+            Clase:"text-success"
+          }
+        ],
+        Stock:245
+      },
+      {
+        Name:"Jane Doe",
+        Description:"Fullstack developer",
+        Price:320.800,
+        Categories:["Category 01","Category 02","Category 03"],
+        Colors:[
+          {
+            Color:"Red",
+            Clase:"text-danger"
+          },
+          {
+            Color:"Blue",
+            Clase:"text-primary"
+          },
+          {
+            Color:"Green",
+            Clase:"text-success"
+          }
+        ],
+        Stock:245
+      }
+    ]
+    let columns = Object.keys(products[0])
 
-          {/* <!-- Content Wrapper --> */}
-          <div id="content-wrapper" className="d-flex flex-column">
+    return (
+      <div className="App">
 
-            {/* <!-- Main Content --> */}
-            <div id="content">
+        <Head />
 
-              <TopBar />
+        <body>
 
-              {/* <!-- Begin Page Content --> */}
-              <div className="container-fluid">
+          <div id="wrapper">
+            
+            <SideBar />
 
-                {/* <!-- Page Heading --> */}
-                <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                  <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+            {/* <!-- Content Wrapper --> */}
+            <div id="content-wrapper" className="d-flex flex-column">
+
+              {/* <!-- Main Content --> */}
+              <div id="content">
+
+                <TopBar />
+
+                {/* <!-- Begin Page Content --> */}
+                <div className="container-fluid">
+
+                  {/* <!-- Page Heading --> */}
+                  <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+                  </div>
+
+                  {/* <!-- Content Row --> */}
+                  <div className="row">
+
+                    {/* <!-- Amount of Products in DB --> */}
+                    {smallBoxes.map((smallBox,i) =>
+                      <SmallBox key={"smallBox" + i} 
+                        title={smallBox.title}
+                        icon={smallBox.icon}
+                        value={smallBox.value}
+                        border={smallBox.border}
+                      />)}
+    
+                  </div>
+
+                  {/* <!-- Content Row --> */}
+                  <div className="row">
+                    {/* <!-- Last Product in DB --> */}
+                    <BigBox title="Last product in Data Dase">
+                      <LastProd />
+                    </BigBox>
+
+                    {/* <!-- Categories in DB --> */}
+                    <BigBox title="Categories in Data Base">
+                      <Categories categories={categories}/>
+                    </BigBox> 
+                  </div>
+
+                  {/* <!-- PRODUCTS LIST --> */}
+                  <h1 className="h3 mb-2 text-gray-800">All the products in the Database</h1>
+                  
+                  {/* <!-- DataTales Example --> */}
+                  <Table columns={columns} products={products}/>
+
                 </div>
-
-                {/* <!-- Content Row --> */}
-                <div className="row">
-
-                  {/* <!-- Amount of Products in DB --> */}
-                  {smallBoxes.map((smallBox,i) =>
-                    <SmallBox key={"smallBox" + i} 
-                      title={smallBox.title}
-                      icon={smallBox.icon}
-                      value={smallBox.value}
-                      border={smallBox.border}
-                    />)}
-  
-                </div>
-
-                {/* <!-- Content Row --> */}
-                <div className="row">
-                  {/* <!-- Last Product in DB --> */}
-                  <BigBox title="Last product in Data Dase">
-                    <LastProd />
-                  </BigBox>
-
-                  {/* <!-- Categories in DB --> */}
-                  <BigBox title="Categories in Data Base">
-                    <Categories categories={categories}/>
-                  </BigBox> 
-                </div>
-
-                {/* <!-- PRODUCTS LIST --> */}
-                <h1 className="h3 mb-2 text-gray-800">All the products in the Database</h1>
-                
-                {/* <!-- DataTales Example --> */}
-                <Table columns={columns} products={products}/>
-
+                {/* <!-- /.container-fluid --> */}
               </div>
-              {/* <!-- /.container-fluid --> */}
-            </div>
-            {/* <!-- End of Main Content --> */}
+              {/* <!-- End of Main Content --> */}
 
-            {/* <!-- Footer --> */}
-            <Footer />
-            {/* <!-- End of Footer --> */}
+              {/* <!-- Footer --> */}
+              <Footer />
+              {/* <!-- End of Footer --> */}
+
+            </div>
+            {/* <!-- End of Content Wrapper --> */}
 
           </div>
-          {/* <!-- End of Content Wrapper --> */}
+          {/* <!-- End of Page Wrapper --> */}
 
-        </div>
-        {/* <!-- End of Page Wrapper --> */}
-
-        </body>
-    </div>
-  );
+          </body>
+      </div>
+    );
+  };
 }
 
 export default App;
